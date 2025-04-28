29c9c5da313282ea69ef1d9c01846f1e70914edd
VictorTadashi <victortadashisaitobarra@gmail.com>
2025-04-28 20:39:27 -0300

    Ajuste na semantica

--- app.py
+++ app.py
@@ -6,27 +6,27 @@ while True:
     print("3 - Ver estatisticas")
     print("4 - Sair do programa")
 
-    opcao = input("Escolha uma opcao: ")
+    opcao = input("Escolha uma opção: ")
 
     match opcao:
         case "1":
             print("Cadastrar alunos")
-            nome = input("Qual e o seu nome? ")
-            idade = int(input("Qual e a sua idade? "))
+            nome = input("Qual é o seu nome? ")
+            idade = int(input("Qual é a sua idade? "))
 
             notas = []
             for i in range(1, 4):
-                nota = float(input(f"Qual e a sua nota {i}? "))
+                nota = float(input(f"Nota {i}? "))
                 notas.append(nota)
 
             media = sum(notas) / 3
-            print("A media do aluno", nome, "e", media)
+            print("A média do aluno", nome, "é", media)
 
             if media >= 7:
                 print("Aprovado")
                 situacao = "Aprovado"
             elif media >= 5:
-                print("Recuperacao")
+                print("Recuperação")
                 situacao = "Recuperacao"
             else:
                 print("Reprovado")
@@ -41,37 +41,36 @@ while True:
             }
 
             alunos.append(aluno)
-            print(f"Aluno {nome} cadastrado com sucesso")
+            print(f"Aluno {nome} cadastrado com sucesso.")
 
         case "2":
             print("Lista de alunos")
             if len(alunos) == 0:
-                print("Nenhum aluno cadastrado")
+                print("Nenhum aluno está cadastrado.")
             else:
                 print("Lista de alunos:")
                 for aluno in alunos:
                     print(f"Nome: {aluno['nome']}, Idade: {aluno['idade']}, Notas: {aluno['notas']}, Media: {aluno['media']}, Situacao: {aluno['situacao']}")
         case "3":
-            print("Ver estatisticas dos alunos")
+            print("Ver estatísticas dos alunos")
             if len(alunos) == 0:
-                print("Nenhum aluno cadastrado")
+                print("Nenhum aluno está cadastrado.")
             else:
                 maior_media = max(aluno['media'] for aluno in alunos)
-                print("Maior media:", maior_media)
+                print("Maior média:", maior_media)
 
-                print("Alunos aporvados:")
+                print("Alunos aprovados:")
                 aprovados = [aluno for aluno in alunos if aluno['situacao'] == "Aprovado"]
 
                 if len(aprovados) == 0:
-                    print("Nenhum aluno aprovado")
+                    print("Nenhum aluno está aprovado.")
                 else:
                     for aluno in aprovados:
-                        print(f"Nome: {aluno['nome']}, Idade: {aluno['idade']}, Notas: {aluno['notas']}, Media: {aluno['media']}, Situacao: {aluno['situacao']}")
+                        print(f"Nome: {aluno['nome']}, Idade: {aluno['idade']}, Notas: {aluno['notas']}, Média: {aluno['media']}, Situação: {aluno['situacao']}")
 
         case "4":
-            print("Sair do programa")
+            print("Saindo do Programa. Até Logo!")
             break
-
 
 
 
